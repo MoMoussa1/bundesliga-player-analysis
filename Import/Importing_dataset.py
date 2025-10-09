a5cc355fe6aa66b06fe255430105f7a101390f15
MoMoussa1 <6307076@edu.vaniercollege.qc.ca>
2025-10-09 09:22:39 -0400

    dataset
    Imported dataset and plotted some graphs

---
+++ Import/Importing_dataset.py
@@ -0,0 +1,29 @@
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+
+#age, height = np.loadtxt("bundesliga_player.csv", delimiter=",", max_rows=1, unpack=True, usecols=(3,4), skiprows=1)
+
+data = pd.read_csv("bundesliga_player.csv")
+
+
+ages = np.array(data["age"])
+market_values = np.array(data["price"])
+height = np.array(data["height"])
+
+
+plt.scatter(ages, market_values)
+
+plt.title("Relationship between ages of Bundesliga players and Market Values")
+plt.xlabel("Age")
+plt.ylabel("Market Value")
+
+plt.show()
+
+plt.scatter(height, market_values)
+
+plt.title("Relationship between heights of Bundesliga players and Market Values")
+plt.xlabel("Height")
+plt.ylabel("Market Value")
+
+plt.show()
