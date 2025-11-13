4e09d5ad4ffcc39d4ad6e197344ca88a88f29e3a
MoMoussa1 <6307076@edu.vaniercollege.qc.ca>
2025-11-13 09:06:52 -0500

    Questions2,3(almost)
    Done question 2, left with one last partin 3, started question 4

---
+++ Import/LIA3.py
@@ -0,0 +1,147 @@
+'''David Tattersall, Mohamed Moussa'''
+
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+
+
+#Importing the data
+
+data = pd.read_csv("bundesliga_player.csv")
+
+##2) Preliminary steps
+
+#Initial data inspection
+
+# a)
+head = data.head()
+print(head) #Print the first 5 rows
+
+
+print(data.shape) #Prints the dimensions of the dataset (rows, columns) (515,17)
+
+
+
+data.info() #Shows the columns which have data that are not empty (non-null), also shows the type of each data (float,int)
+# missing values:
+# 286 full_name
+# 5 place_of_birth
+# 5 price
+# 5 max_price
+# 9 foot
+# 34 contract_expires
+# 27 player_agent
+# 322 outfitter
+
+# c)
+
+#Filled numeric columns with median for some prices and max prices since it was only 5 that were missing for both, and the median represents the data relatively well
+data["price"] = data["price"].fillna(data["price"].median())
+data["max_price"] = data["max_price"].fillna(data["max_price"].median())
+
+#Did not drop because we wouldve lost a lot of data. Instead, just replaced with NA so that we can still use other data from the same row
+data["outfitter"] = data["outfitter"].fillna("NA")
+data["player_agent"] = data["player_agent"].fillna("NA")
+data["foot"] = data["foot"].fillna("NA")
+data["full_name"] = data["full_name"].fillna("NA")
+data["place_of_birth"] = data["place_of_birth"].fillna("NA")
+
+# a)
+dataset_description = data.describe()
+print(dataset_description) #Shows mean, std, statistical summary
+
+# b)
+
+duplicates = data.duplicated()
+
+count_duplicates = duplicates.value_counts()
+print(count_duplicates) # no duplicates
+
+##3) Univariate non-graphical EDA
+
+#Manipulating Numerical Data
+
+numerical_data = data[["age", "height", "price", "max_price", "shirt_nr"]]
+
+print("-----Mean-----")
+print(numerical_data.mean())
+
+print("-----Median-----")
+print(numerical_data.median())
+
+print("-----Mode-----")
+print(numerical_data.mode())
+
+print("-----Standard Deviation-----")
+print(numerical_data.std())
+
+print("-----Variance-----")
+print(numerical_data.var())
+
+print("-----Skew-----")
+print(numerical_data.skew())
+
+print("-----Kurtosis-----")
+print(numerical_data.kurtosis())
+
+print("-----Quartiles-----")
+print(numerical_data.quantile([0.25,0.5, 0.75]))
+
+# Manipulating Categorical Data
+
+categorical_data = data[["name", "full_name", "nationality", "place_of_birth", "position", "foot", "club", "contract_expires", "joined_club", "player_agent", "outfitter"]]
+
+print("-----Mode-----")
+print(categorical_data.mode())
+
+
+##4) Univariate graphical EDA
+'''
+#a) bins
+
+sns.displot(numerical_data, x="age", binwidth=2)
+
+sns.displot(numerical_data, x="height", binwidth=0.05)
+
+sns.displot(numerical_data, x="price", binwidth=5)
+
+sns.displot(numerical_data, x="max_price", binwidth=10)
+
+sns.displot(numerical_data, x="shirt_nr", binwidth=2)
+
+#b) conditioning
+
+sns.displot(data, x="age", hue="position")
+
+sns.displot(data, x="height", hue="position")
+
+sns.displot(data, x="price", hue="position")
+
+sns.displot(data, x="max_price", hue="position")
+
+sns.displot(data, x="shirt_nr", hue="position")
+
+#c) stacked histograms
+
+sns.displot(data, x="age", hue="position", multiple="stack")
+
+sns.displot(data, x="height", hue="position", multiple="stack")
+
+sns.displot(data, x="price", hue="position", multiple="stack")
+
+sns.displot(data, x="max_price", hue="position", multiple="stack")
+
+sns.displot(data, x="shirt_nr", hue="position", multiple="stack")
+'''
+#d) dodge bars
+
+sns.displot(data, x="price", hue="position", multiple="dodge", bins=3)
+
+sns.displot(data, x="height", hue="position", multiple="dodge", binwidth= 0.05)
+
+data["top_6_clubs"] = data[data["club"].isin(["Bayern Munich", "Bor. Dortmund", "RB Leipzig", "B. Leverkusen", "TSG Hoffenheim", "VfB Stuttgart"])]
+data_top5_age = 
+sns.displot(data, x="age", hue="top_6_clubs", multiple="dodge")
+
